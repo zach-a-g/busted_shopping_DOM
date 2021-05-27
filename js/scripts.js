73a9362128c79dac96f9deb52b7a726addecb667
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-05-27 10:34:19 -0500

    Fixed Broken Code

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,63 @@
 'use strict';
 
-document.addEventListener('DMContentLoaded', function () {
+// THIS IS THE BROKEN CODE
+
+// document.addEventListener('DMContentLoaded', function () {
+//     console.log('Content Loaded');
+//     const myList = document.querySelector('#myList');
+
+
+//     generateList.addEventListener('click', function () {
+//         const inputItems = document.querySelector('input');
+//         const listElement = document.createElement('ul');
+
+//         inputItems.forEach(function (inputItem) {
+//             const listItem = document.createElement('li');
+//             listItem.innerText = inputItem.value;
+//             listElements.append(listItem);
+//         });
+
+//     });
+// });
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// THIS IS THE FIXED CODE
+
+document.addEventListener('DOMContentLoaded', function () {
     console.log('Content Loaded');
     const myList = document.querySelector('#myList');
+    const generateList = document.querySelector('#generateList');
 
 
     generateList.addEventListener('click', function () {
-        const inputItems = document.querySelector('input');
+        const inputItems = document.querySelectorAll('.input');
         const listElement = document.createElement('ul');
-
-        inputItems.forEach(function (inputItem) {
+        console.log(inputItems);
+        myList.append(listElement);
+        
+        inputItems.forEach(function (inputItems) {
             const listItem = document.createElement('li');
-            listItem.innerText = inputItem.value;
-            listElements.append(listItem);
+            listItem.innerText = inputItems.value;
+            listElement.append(listItem);
         });
-
     });
 });
+
+// THIS IS MY OTHER WAY TO FIX THE CODE ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// const generateListButton = document.querySelector('#generateList');
+// const myList = document.querySelector('#myList');
+// const inputs = document.querySelectorAll('.input')
+
+
+// generateListButton.addEventListener('click', function () {
+//     console.log('Content Loaded');
+//     inputs.forEach(function (item) {
+//         const listItem = document.createElement('ul');
+//         listItem.innerText = item.value;
+//         myList.append(listItem);
+//         });
+// })
+  
+
